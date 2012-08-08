05fa5447508fb58fd0927e315f10d4141a31d898
Alex Recarey <ar@cyberfonica.com>
2012-08-08 16:24:10 +0200

    added new packages to personal config

--- personal/packages.el
+++ personal/packages.el
@@ -12,11 +12,25 @@
     prelude-python
     prelude-ruby
     prelude-xml
-    pony-mode ; Mode for editing django files
-    lua-mode ; Mode for editing lua
-    less-css-mode ; Edit less css files
-    scala-mode ; Mode for editing scala
-    org ; Outline based notes and organization
+    prelude-scss   ; edit SCSS files (Sass)
+    pony-mode      ; Mode for editing django files
+    lua-mode       ; Mode for editing lua
+    less-css-mode  ; Edit less css files
+    scala-mode     ; Mode for editing scala
+    org            ; Outline based notes and organization
+    flymake-coffee ; flymake extensions for coffescript
+    flymake-css    ; flymake for css
+    flymake-cursor ; flymake error messages in minibufer
+    flymake-haml   ; flymake handler for haml files
+    flymake-jslint ; flymake support for javascript using jslint
+    flymake-php    ; flymake for php
+    flymake-python-pyflakes ; flymake for python
+    flymake-ruby   ; flymake for ruby
+    flymake-sass   ; flymake for sass
+    flymake-shell  ; flymake for bash
+    csv-mode       ; edit and navigate csv files
+    nginx-mode     ; editing nginx configuration files
+    nhexl-mode     ; edit hex dump files
    )
 )
 
@@ -28,3 +42,19 @@
 (dolist (package personal-package-list)
   (when (not (package-installed-p package))
     (package-install package)))
+
+; Test packages list
+; These packages are packages I found interesting and want to test
+'(google-maps
+  google-this
+  google-translate
+  handlebars-mode
+  jinja2-mode
+  js-comint ; javascript terminal
+)
+
+; Packages to package with melpa
+'(chop.el
+  pydoc-info
+  ensime
+)
